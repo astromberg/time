d23c3f3fe886743bfd985de416a0eee8ad028601
astromberg <someone@example.com>
2014-11-15 18:31:46 -0800

    Clean up old code
    There was a bunch of commented out code that I wanted to remove, but
    have in an earlier revision in case I felt inclined to bring it back
    later.

--- background.js
+++ background.js
@@ -1,10 +1,6 @@
 // This method is called when a message is passed. It should provide the selected text.
 var exists = false;
 function onRequest(request, sender, sendResponse) {
-  // Show the page action for the tab that the sender (content script)
-  // was on.
-  // chrome.pageAction.show(sender.tab.id);
-
   if (!request['valid']) {
     chrome.contextMenus.remove('timestamp');
 	exists = false;
@@ -31,15 +27,3 @@ function onRequest(request, sender, sendResponse) {
 
 // Listen for the content script to send a message to the background page (this script).
 chrome.extension.onMessage.addListener(onRequest);
-
-// This is if I wanted someone to update the whole page. Maybe in the future I will bring this back.
-/* chrome.pageAction.onClicked.addListener(function(tabs) {
-  chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-  if (chrome.runtime.lastError) {
-      console.log(chrome.runtime.lastError.message);
-    }
-    chrome.tabs.sendMessage(tabs[0].id, {}, function(response) {});
-  });
-});
-*/
-
